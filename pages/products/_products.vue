321298f228d88250dacb9245c2229247b97fbc0d
mohit <mohit.dhiman.sarkaghat@gmail.com>
2021-08-17 16:51:00 +0530

    page to add products folder

---
+++ pages/products/_products.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Nav class="sticky top-0" />
+    <h1 class="font-bold m-5 md:mx-10">
+      {{ currentProduct.name }}
+    </h1>
+    <Products :data="currentProduct" />
+    <Ads class="mx-auto sm:m-10" />
+    <Footer />
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ $strapi, route }) {
+    const id = route.params.products
+    const currentProduct = await $strapi.$products.findOne(id)
+    return { currentProduct }
+  },
+  data() {
+    return {
+      currentProduct: {},
+    }
+  },
+}
+</script>
+<style scoped></style>
