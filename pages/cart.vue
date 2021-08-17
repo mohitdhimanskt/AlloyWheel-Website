7d556e88986247678054025f9ed57b2e3a97f4d2
mohit <mohit.dhiman.sarkaghat@gmail.com>
2021-08-17 17:41:19 +0530

    Changes to pages

--- pages/cart.vue
+++ pages/cart.vue
@@ -10,7 +10,7 @@
       class="w-4/5 sm:w-1/2 flex items-center space-x-3 mx-auto shadow-lg m-5 p-3"
     >
       <div>
-        <img class="h-24" :src="`${item.url}`" alt="" />
+        <img class="h-24" :src="`http://localhost:1337${item.url}`" alt="" />
       </div>
       <div>
         <p>
@@ -19,9 +19,7 @@
         <p>
           {{ item.quantity | formatQuantity }}
         </p>
-        <button class="button--delete" @click="deleteCartItem(item.id)">
-          Delete
-        </button>
+        <p @click="deleteCartItem(item.id)">Delete</p>
       </div>
     </div>
     <div class="w-4/5 sm:w-1/2 mb-2 mx-auto">
@@ -40,11 +38,17 @@
     <Footer />
   </div>
 </template>
+
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import { loadStripe } from '@stripe/stripe-js'
 export default {
   data() {
     return {
+      dataItems: {},
+      session: {},
+      stripe: {},
+      stripePromise: {},
     }
   },
   computed: {
@@ -52,6 +56,59 @@ export default {
   },
   mounted() {
     this.displayMessage()
+  },
+  methods: {
+    async handleSubmit(e) {
+      //   console.log(JSON.stringify(this.dataItems))
+      e.preventDefault()
+      const response = await this.$http.$post(`http://localhost:1337/orders`, {
+        cartDetail: this.getCart,
+        cartTotal: this.getCartTotal.toFixed(2),
+      })
+      this.$swal({
+        title: 'Please wait',
+        text: 'redirecting you to stripe, click ok',
+        icon: 'success',
+        button: 'Ok',
+      })
+      // stripe logic
+      const stripePromise = loadStripe(STRIPE_KEY)
+      const session = response
+      const stripe = await stripePromise
+      const result = await stripe.redirectToCheckout({
+        sessionId: session.id,
+      })
+      console.log(response)
+      if (result.error) {
+        this.$nuxt.context.error(result.error.message)
+      }
+    },
+    // using vue-swal to display messages
+    displayMessage() {
+      if (this.$route.query.success) {
+        this.$swal({
+          title: 'Order placed!',
+          text: 'You will receive an email confirmation.',
+          icon: 'success',
+          button: 'Ok',
+        })
+      } else if (this.$route.query.canceled) {
+        this.$swal({
+          title: 'Order canceled!',
+          text: "continue to shop around and checkout when you're ready.",
+          icon: 'warning',
+          button: 'Ok',
+        })
+      }
+    },
+    formatCartTotal(num) {
+      if (num > 0) {
+        return num.toFixed()
+      } else {
+        return num
+      }
+    },
+    ...mapActions(['deleteCartItem']),
   },
   filters: {
     formatPrice(price) {
@@ -64,4 +121,5 @@ export default {
   },
 }
 </script>
+
 <style scoped></style>
